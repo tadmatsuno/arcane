3383e51df4e7fdf060e1ef4ca52760275cd0079d
Tadafumi Matsuno <tadafumi.mn@gmail.com>
2025-10-01 14:37:21 +0200

    update test scripts

--- test/test_modelatm.py
+++ test/test_modelatm.py
@@ -1,8 +1,8 @@
 from arcane.mdlatm import marcs,avg3d
 import unittest
 import os
-from outputdir import check_temp_dir, clear_temp_dir
 
+outputdir  = os.path.join(os.path.dirname(__file__),'output')
 
 class TestModelAtm(unittest.TestCase):
     def test_MARCS(self):
@@ -10,9 +10,9 @@ class TestModelAtm(unittest.TestCase):
         # Test MARCS model atmosphere class
         sun = marcs.get_marcs_mod(5777,4.44,0.0,0.0)
         self.assertIsNotNone(sun)
-        sun.write('output/sun_marcs.mod')
-        self.assertTrue(os.path.exists('output/sun_marcs.mod'))
-        sun2 = marcs.read_marcs('output/sun_marcs.mod')
+        sun.write(os.path.join(outputdir, 'sun_marcs.mod'))
+        self.assertTrue(os.path.exists(os.path.join(outputdir, 'sun_marcs.mod')))
+        sun2 = marcs.read_marcs(os.path.join(outputdir, 'sun_marcs.mod'))
         self.assertEqual(sun['ndepth'], sun2['ndepth'])
         self.assertAlmostEqual(sun['T'][0], sun2['T'][0], places=1)
 
@@ -22,9 +22,9 @@ class TestModelAtm(unittest.TestCase):
         '''
         sun = avg3d.get_model(5777,4.44,0.0,0.0)
         self.assertIsNotNone(sun) 
-        sun.write('output/sun_avg3d.mod')
-        self.assertTrue(os.path.exists('output/sun_avg3d.mod'))
-        sun2 = avg3d.read_avg3d('output/sun_avg3d.mod')
+        sun.write(os.path.join(outputdir, 'sun_avg3d.mod'))
+        self.assertTrue(os.path.exists(os.path.join(outputdir, 'sun_avg3d.mod')))
+        sun2 = avg3d.read_avg3d(os.path.join(outputdir, 'sun_avg3d.mod'))
         self.assertEqual(sun['ndepth'], sun2['ndepth'])
         self.assertAlmostEqual(sun['T'][0], sun2['T'][0], places=1)
         
